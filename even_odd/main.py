ab0f05a5015d0e43823927b2a425d9d277b96ac6
wellinton <wellinton1067@gmail.com>
2023-01-06 21:26:33 -0300

    including divisible number function

--- even_odd/main.py
+++ even_odd/main.py
@@ -1,5 +1,5 @@
 
-print("WELCOME EVEN OR ODD RANGE")
+menu = int(input("WELCOME EVEN OR ODD RANGE\nCHOOSE:\n1 - EVEN OR ODD\n2 - DIVISIBLE NUMBER\n"))
 
 def evenOrOdd():
     list1 = [* range(int(input("First number of range: ")), int(input("Second number of range: ")), 1)]
@@ -14,8 +14,21 @@ def evenOrOdd():
                 list1.remove(i)
     else:
         print("invalid input!!!!!")
-        list1 = []
+        list1.clear()
         evenOrOdd()
     print(*list1, sep="-")
 
-evenOrOdd()
+def divisibleNumb():
+    list2 = [* range(int(input("First number of range: ")), int(input("Second number of range: ")), 1)]
+    divisibleNumber = int(input("Choose a number to be divisible: "))
+    list3 = []
+    for i in list2:
+        if i % divisibleNumber == int(0):
+            list3.append(i)
+    print(*list3, sep="-")
+
+
+if menu == 1:
+    evenOrOdd()
+else:
+    divisibleNumb()
