1d888ea261d848ac6b8220e851dca00c93066baa
wellinton <wellinton1067@gmail.com>
2023-01-08 14:46:52 -0300

    including another way to find highest even

---
+++ even_odd/highest_even2.py
@@ -0,0 +1,13 @@
+
+def highestEven(li):
+    highest = 0
+    for i in range(1, len(li)):
+        for j in range(0, len(li)):
+            if li[j] > li[i] and  li[j] % 2 == 0:
+                highest = li[j]
+    return highest
+
+
+
+list1 = [1,2,3,48,49,6,7,8,9,10]
+print(highestEven(list1))
