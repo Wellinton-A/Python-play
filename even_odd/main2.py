bf349bc6650e5709643570ed6f1eb8c3a13c3809
wellinton <wellinton1067@gmail.com>
2023-01-08 22:46:32 -0300

    add exercises

--- even_odd/main2.py
+++ even_odd/main2.py
@@ -1,3 +1,16 @@
-for i,char in enumerate(list(range(1, 100))):
-    if char == 50:
-        print(i)
+list1 = ['a','b','c','d']
+
+list2 = list1[::-1]
+
+list1.append('e')
+
+print(*list1)
+print(*list2)
+
+print(id(list1))
+print(id(list2))
+
+array1 = "!"
+
+print(array1.join("Wellinton"))
+
